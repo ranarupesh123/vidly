a61b8ebeeef13bc960ac187ca6c4c01739b08a4c
ranarupesh <rupeshrana288@gamil.com>
2021-01-21 22:12:14 +0530

    created user and genre API

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const uri = require("./uri");
 
 const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 
 const port = 5000 || process.env.PORT;
-const URI =
-  "mongodb+srv://rupesh:rana@cluster0.lo1ni.mongodb.net/vidly?retryWrites=true&w=majority";
-
+const URI = uri;
 app.use(express.json());
 app.use("/", home);
 app.use("/api/genres", genres);
---
+++ uri.js
@@ -0,0 +1,4 @@
+var uri =
+  "mongodb+srv://rupesh:rana@cluster0.lo1ni.mongodb.net/vidly?retryWrites=true&w=majority";
+
+exports.uri = uri;
